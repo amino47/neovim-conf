4b2fb0d89bbb204bc43374a3bd7abeaa7c803af1
Riley <riley@example.com>
2026-09-12 13:28:58 -0700

    existing config

---
+++ plugin/conf.lua
@@ -0,0 +1,11 @@
+vim.cmd[[colorscheme tokyonight]]
+vim.opt.nu = true
+vim.opt.relativenumber = false
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+require("ibl").setup()
+
